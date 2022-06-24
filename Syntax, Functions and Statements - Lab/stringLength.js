45a7748a5f215b734e392bde10c5415b8862b208
dechevgfx <101424579+dechevgfx@users.noreply.github.com>
2022-06-25 00:55:17 +0300

    Add files via upload

---
+++ Syntax, Functions and Statements - Lab/stringLength.js
@@ -0,0 +1,6 @@
+function solve(strOne, strTwo, strThree) {
+    let result = strOne.length + strTwo.length + strThree.length
+    let average = Math.floor(result / 3)
+    console.log(result)
+    console.log(average)
+}
