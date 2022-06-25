3529aa54c57dc4bc193be498ad655c5c82347305
Georgi Dechev <georgiddechev@gmail.com>
2022-06-25 09:33:18 +0300

    Add files via upload

---
+++ Syntax, Functions and Statements - Exercies/previousDay.js
@@ -0,0 +1,6 @@
+function solve(year, month, day) {
+let dateString = year + '-' + month + '-' + day
+    let date = new Date(dateString)
+    date.setDate(day-1)
+    console.log(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`)
+}solve(2016, 10, 1)
