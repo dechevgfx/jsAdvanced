101929eb25a797877230f7aad8601d3a5584d164
Georgi Dechev <georgiddechev@gmail.com>
2022-07-13 14:10:47 +0300

    Update circleArea.js

--- Syntax, Functions and Statements - Lab/circleArea.js
+++ Syntax, Functions and Statements - Lab/circleArea.js
@@ -1,1 +1,11 @@
-func
+function circle(input) {
+    let type = typeof input;
+    if (type !== "number") {
+        console.log(
+            `We can not calculate the circle area, because we receive a ${type}.`,
+        );
+    } else {
+        let area = Math.PI * Math.pow(input, 2);
+        console.log(area.toFixed(2));
+    }
+}
