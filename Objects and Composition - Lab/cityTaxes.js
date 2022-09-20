e0be1d5e81f2521bf6c71dd5db0a9a8c64ab40a7
Georgi Dechev <georgiddechev@gmail.com>
2022-09-20 13:54:42 +0300

    Add files via upload

---
+++ Objects and Composition - Lab/cityTaxes.js
@@ -0,0 +1,20 @@
+    function information(name, population, treasury) {
+        let record = {
+            name: name,
+            population: population,
+            treasury: treasury,
+            taxRate: 10,
+            collectTaxes(){
+                this.treasury += this.population * this.taxRate;
+            },
+            applyGrowth(percentage){
+                percentage /= 100;
+                this.population *= percentage + 1
+            },
+            applyRecession(percentage){
+                percentage /= 100;
+                this.treasury *= (1 - percentage)
+            }
+        }
+        return record;
+    }
