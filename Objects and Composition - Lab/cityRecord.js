6cafa7c8bd58067b57fa4926703dcdaff52961ee
Georgi Dechev <georgiddechev@gmail.com>
2022-09-20 13:54:16 +0300

    Add files via upload

---
+++ Objects and Composition - Lab/cityRecord.js
@@ -0,0 +1,11 @@
+function information(name, population, treasury) {
+    // let record = {
+    //     name: name,
+    //     population: population,
+    //     treasury: treasury,
+    // }
+
+    let record = { name, population, treasury }
+
+    return record;
+}
