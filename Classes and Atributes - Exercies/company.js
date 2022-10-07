34f883739148df1dedaf7ac9d4d65c9c5a0b3f4c
Georgi Dechev <georgiddechev@gmail.com>
2022-10-07 14:44:46 +0300

    Add files via upload

---
+++ Classes and Atributes - Exercies/company.js
@@ -0,0 +1,81 @@
+class Company {
+    constructor() {
+        this.departments = {};
+    }
+
+    addEmployee(name, salary, position, department) {
+        if (
+            !name ||
+            !salary ||
+            !position ||
+            !department ||
+            Number(salary) < 0
+        ) {
+            throw new Error("Invalid input!");
+        }
+
+        if (!this.departments[department]) {
+            this.departments[department] = {};
+            this.departments[department].employees = [];
+        }
+
+        const employee = Object.assign({
+            name,
+            salary: Number(salary),
+            position,
+        });
+        this.departments[department].employees.push(employee);
+
+        return `New employee is hired. Name: ${name}. Position: ${position}`;
+    }
+
+    bestDepartment() {
+        let bestDepartmentAvgSalary = 0;
+        let bestDepartment;
+        let bestDepartmentName;
+        for (const department in this.departments) {
+            this.departments[department].avgSalary =
+                this.departments[department].employees.reduce(
+                    (a, b) => a + b.salary,
+                    0,
+                ) / this.departments[department].employees.length || 0;
+
+            if (
+                this.departments[department].avgSalary > bestDepartmentAvgSalary
+            ) {
+                bestDepartmentAvgSalary =
+                    this.departments[department].avgSalary;
+                bestDepartment = this.departments[department];
+                bestDepartmentName = department;
+            }
+        }
+
+        bestDepartment.employees.sort(
+            (a, b) => b.salary - a.salary || a["name"].localeCompare(b["name"]),
+        );
+
+        return `Best Department is: ${bestDepartmentName}\nAverage salary: ${bestDepartmentAvgSalary.toFixed(
+            2,
+        )}\n${bestDepartment.employees
+            .map((x) => `${x.name} ${x.salary} ${x.position}`)
+            .join("\n")}`;
+    }
+}
+let c = new Company();
+
+c.addEmployee("dsfsdf", 2000, "engineer", "Construction");
+
+c.addEmployee("Pesho", 1500, "electrical engineer", "Construction");
+
+c.addEmployee("Slavi", 500, "", "Construction");
+
+c.addEmployee("Stan", 2000, "architect", "Construction");
+
+c.addEmployee("Stanimir", 10, "digital marketing manager", "Marketing");
+
+c.addEmployee("Pesho", 1000, "graphical designer", "Marketing");
+
+c.addEmployee("Gosho", 1350, "HR", "Human resources");
+
+
+console.log(c.bestDepartment());
